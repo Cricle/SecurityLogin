513860b021e5a9a1999cb023f34257844a3340c1
HCricle <none>
2022-03-06 00:17:22 +0800

    fast redis convert

--- src/SecurityLogin.Redis/Converters/PoolEncoding.cs
+++ src/SecurityLogin.Redis/Converters/PoolEncoding.cs
@@ -1,4 +1,5 @@
-﻿using System.Buffers;
+﻿using System;
+using System.Buffers;
 using System.Text;
 
 namespace SecurityLogin.Redis.Converters
@@ -15,6 +16,21 @@ namespace SecurityLogin.Redis.Converters
         }
         public static PoolBuffer GetBytes(string s,Encoding encoding,ArrayPool<byte> pool)
         {
+            if (s==null)
+            {
+                throw new ArgumentNullException(nameof(encoding));
+            }
+
+            if (encoding is null)
+            {
+                throw new ArgumentNullException(nameof(encoding));
+            }
+
+            if (pool is null)
+            {
+                throw new ArgumentNullException(nameof(pool));
+            }
+
             var byteCount = encoding.GetByteCount(s);
             var bytes = pool.Rent(byteCount);
             int bytesReceived = encoding.GetBytes(s, 0, s.Length, bytes, 0);
--- src/SecurityLogin.Redis/RedisConvertExtensions.cs
+++ src/SecurityLogin.Redis/RedisConvertExtensions.cs
@@ -5,12 +5,41 @@ namespace StackExchange.Redis
 {
     public static class RedisConvertExtensions
     {
+        private static readonly Type StringType = typeof(string);
+        private static readonly Type RedisValueType = typeof(RedisValue);
+        private static readonly Type DateTimeType = typeof(DateTime);
+
         public static T Get<T>(this in RedisValue value)
         {
             return (T)Get(value, typeof(T));
         }
-        public static object Get(this in RedisValue value,Type type)
+        public static object Get(this in RedisValue value, Type type)
         {
+            if (type == RedisValueType)
+            {
+                return value;
+            }
+            if (type.IsPrimitive || type == DateTimeType)
+            {
+                return Convert.ChangeType(value, type);
+            }
+            if (type == StringType)
+            {
+                return value.ToString();
+            }
+            var underType = Nullable.GetUnderlyingType(type);
+
+            if (underType != null)
+            {
+                try
+                {
+                    return Get(in value, underType);
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
             var convert = KnowsRedisValueConverter.GetConverter(type);
             return convert?.ConvertBack(value, null);
         }
